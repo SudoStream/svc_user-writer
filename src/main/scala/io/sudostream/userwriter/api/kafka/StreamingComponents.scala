cf4dedbf2d798f3d391343bc42801bb715cc2b9b
Andy Boyle <andy@sudostream.io>
2018-01-09 15:26:09 +0000

    0.0.1-20 : Add SASL JASS configuration to service

--- src/main/scala/io/sudostream/userwriter/api/kafka/StreamingComponents.scala
+++ src/main/scala/io/sudostream/userwriter/api/kafka/StreamingComponents.scala
@@ -5,6 +5,8 @@ import akka.kafka.ProducerSettings
 import akka.stream.Materializer
 import io.sudostream.timetoteach.kafka.serializing.SystemEventSerializer
 import io.sudostream.userwriter.config.{ActorSystemWrapper, ConfigHelper}
+import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.common.config.SaslConfigs
 import org.apache.kafka.common.serialization.ByteArraySerializer
 
 import scala.concurrent.ExecutionContextExecutor
@@ -16,14 +18,21 @@ class StreamingComponents(configHelper: ConfigHelper, actorSystemWrapper: ActorS
   val log = system.log
 
   lazy val kafkaProducerBootServers = configHelper.config.getString("akka.kafka.producer.bootstrapservers")
+  lazy val kafkaProducerSaslJaasUsername = configHelper.config.getString("akka.kafka.producer.saslJassUsername")
+  lazy val kafkaProducerSaslJaasPassword = configHelper.config.getString("akka.kafka.producer.saslJassPassword")
+  lazy val kafkaProducerSaslJaasConfig = s"org.apache.kafka.common.security.scram.ScramLoginModule required " +
+    s"""username="$kafkaProducerSaslJaasUsername" password="$kafkaProducerSaslJaasPassword";"""
 
   val producerSettings = ProducerSettings(system, new ByteArraySerializer, new SystemEventSerializer)
     .withBootstrapServers(kafkaProducerBootServers)
+    .withProperty(SaslConfigs.SASL_JAAS_CONFIG, kafkaProducerSaslJaasConfig)
+    .withProperty(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-256")
+    .withProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL")
+
+  log.info(s"Systems event topic is '$definedSystemEventsTopic'")
 
   def definedSystemEventsTopic: String = {
-    val sink_topic = configHelper.config.getString("user-reader.system_events_topic")
-    log.info(s"Sink topic is '$sink_topic'")
-    sink_topic
+    configHelper.config.getString("user-writer.system_events_topic")
   }
 
 }
